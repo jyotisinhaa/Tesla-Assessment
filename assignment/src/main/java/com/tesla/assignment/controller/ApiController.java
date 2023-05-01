0b1d373b35cab0e91d3259fcd5dc7cd110f74390
Jyoti Sinha <jyotisinhabit@gmail.com>
2023-04-30 20:02:15 -0400

    Revert "Refactor code"
    This reverts commit 1406877a45b22bf07aeddd4e6d766a6e4ade9c95.

--- assignment/src/main/java/com/tesla/assignment/controller/ApiController.java
+++ assignment/src/main/java/com/tesla/assignment/controller/ApiController.java
@@ -1,7 +1,7 @@
 package com.tesla.assignment.controller;
+import com.tesla.assignment.entity.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -12,61 +12,58 @@ import java.util.Map;
 public class ApiController {
     private final List<String> errorList = new ArrayList<>();
     @PostMapping(value = "/temp")
-    public ResponseEntity<Map<String, Object>> postData(@RequestBody(required = false) Map<String, Object> payload){
-
-        // If the payload is null,empty, doesn't contain data parameter then throw the error
-        if (payload == null || payload.isEmpty() || payload.get("data") == null) {
-            return ResponseEntity.badRequest().body(Map.of("error", "bad request"));
+    public ResponseEntity<Map<String, Object>> postData(@RequestBody Data payload){
+        int deviceId = 0;
+        long epoch = 0;
+        double temperature = 0;
+        String payloadString = payload.getData();
+        //If the data is empty then return the error
+        if (payloadString.isEmpty()){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", "bad request"));
         }
+        String[] chunk= payloadString.split(":");
 
         try{
-            String payloadString = (String) payload.get("data");
-            if ( payloadString == null || payloadString.isEmpty()  || payloadString.trim().isEmpty()){
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", "bad request"));
-            }
-            String[] chunk= payloadString.split(":");
-            //If the length of the chunk is not 4 or the third element doesn't contain 'Temperature' keyword then we need to throw an error
-            if (chunk.length!=4 || !chunk[2].contains("'Temperature'")){
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", "bad request"));
-            }
-            int deviceId = 0;
-            long epoch = 0;
-            double temperature = 0.0;
-            // Handling the format of deviceId, epoch, and temperature
-            try{
-                deviceId = Integer.parseInt(chunk[0]);
-                epoch = Long.parseLong(chunk[1]);
-                temperature =  (chunk[3].contains("."))?Double.parseDouble(chunk[3]):0.0;
-
-            }
-            catch (NumberFormatException e) {
-                errorList.add(payloadString);
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", "bad request"));
-
-            }
-            if (deviceId==0||epoch==0||temperature==0.0){
-                errorList.add(payloadString);
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", "bad request"));
-            }
-            //Check for the over temperature
-            if (temperature>= 90){
-                String formattedTime = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(epoch);
-                return ResponseEntity.ok().body(Map.of(
-                        "overtemp", true,
-                        "device_id", deviceId,
-                        "formatted_time",formattedTime
-                ));
-            }
-            else {
-                return ResponseEntity.ok().body(Map.of("overtemp", "false"));
+            deviceId = Integer.parseInt(chunk[0]);
+        }
+        catch (NumberFormatException e) {
+            errorList.add(chunk[0]);
+        }
+        try {
+            epoch = Long.parseLong(chunk[1]);
+        }catch(NumberFormatException e){
+            errorList.add(chunk[1]);
+        }
+        try{
+            if(chunk[3].contains(".")){
+                temperature = Double.parseDouble(chunk[3]);
+            } else{
+                errorList.add(chunk[3]);
             }
         }
-        catch (HttpMessageNotReadableException e) {
-            return ResponseEntity.badRequest().body(Map.of("error", "bad request"));
+        catch(NumberFormatException e){
+            errorList.add(chunk[3]);
+        }
+
+        //If the length of the chunk is not 4 or the third element doesn't contain 'Temperature' keyword then we need to throw an error
+        if (chunk.length!=4 || !chunk[2].contains("Temperature") ||deviceId==0||epoch==0||temperature==0.0){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", "bad request"));
+        }
+
+        //Check for the over temperature
+        if (temperature>= 90){
+            String formattedTime = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(epoch);
+            return ResponseEntity.ok().body(Map.of(
+                    "overtemp", true,
+                    "device_id", deviceId,
+                    "formatted_time",formattedTime
+            ));
+        }
+        else{
+            return ResponseEntity.ok().body(Map.of("overtemp", "false"));
         }
 
     }
-
     @GetMapping("/errors")
     public ResponseEntity<Map<String, Object>> getErrors(){
         List<String> errors = new ArrayList<>(errorList);
---
+++ assignment/src/main/java/com/tesla/assignment/entity/Data.java
@@ -0,0 +1,13 @@
+package com.tesla.assignment.entity;
+
+public class Data {
+    public String Data;
+
+    public String getData() {
+        return Data;
+    }
+
+    public void setData(String data) {
+        Data = data;
+    }
+}
